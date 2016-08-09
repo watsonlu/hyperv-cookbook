03f0443a7f4920581c3eaa2bfe98096d19eb2122
Luke Watson <Luwatson90@gmail.com>
2016-08-09 14:45:17 -0400

    Update recipe to read hash

--- recipes/create.rb
+++ recipes/create.rb
@@ -1,5 +1,9 @@
-powershell_script "create a vm" do
-  code <<-EOH
-New-VM -Name "#{node["hyperv"]["name"]}" -MemoryStartupBytes #{node["hyperv"]["create"]["memory"]} -SwitchName "#{node["hyperv"]["create"]["switch"]}" -VHDPath "#{node["hyperv"]["create"]["vhd"]}"
-EOH
+vm_list = node["hyperv"]["vm_list"]
+
+vm_list.each do |item|
+  powershell_script "create a vm" do
+    code <<-EOH
+  New-VM -Name "#{item.name}" -MemoryStartupBytes #{item.memory} -SwitchName "#{node["hyperv"]["switch"]}" -VHDPath "#{item.vhd}"
+  EOH
+  end
 end
