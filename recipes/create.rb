3e70c8cfb1364541b9a7129e3c7dbe16ddcf7ebb
Luke Watson <Luwatson90@gmail.com>
2016-08-10 11:38:57 -0400

    action should be create

--- recipes/create.rb
+++ recipes/create.rb
@@ -9,6 +9,7 @@ vm_list.each do |item|
     remote_file "Copy source file to destination" then
       path item.destination_vhd
       source source_vhd
+      action :create
     end
   end
 
@@ -17,5 +18,5 @@ vm_list.each do |item|
   New-VM -Name "#{item.name}" -MemoryStartupBytes #{item.memory} -SwitchName "#{node["hyperv"]["switch"]}" -VHDPath "#{item.destination_vhd}"
   EOH
   end
-  
+
 end
