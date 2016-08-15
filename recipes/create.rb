c13030cfab98e51bec53da9427583368f90b4026
Luke Watson <Luwatson90@gmail.com>
2016-08-15 16:15:05 -0400

    update variable name for hash

--- recipes/create.rb
+++ recipes/create.rb
@@ -1,18 +1,16 @@
-node["hyperv"]["machines_to_create"].each do |virtualmachine|
-  log virtualmachine
-  parent_dir = File.dirname(virtualmachine.destination_vhd)
-  directory 'Create the source directory' do
-    path parent_dir
-    recursive true
-    :create
-  end
+def copy_with_path(src, dst)
+  FileUtils.mkdir_p(File.dirname(dst))
+  FileUtils.cp(src, dst)
+end
 
+node["hyperv"]["machines"].each do |virtualmachine|
+  log virtualmachine
   if Pathname.new(virtualmachine.destination_vhd).file? then
     log "VHD already exists, skipping (#{virtualmachine.destination_vhd})"
     next
   end
   if !virtualmachine.source_vhd.to_s.empty? then
-    FileUtils.cp(virtualmachine.source_vhd, virtualmachine.destination_vhd)
+    copy_with_path(virtualmachine.source_vhd, virtualmachine.destination_vhd)
   end
 
   powershell_script "create a vm" do
@@ -20,5 +18,4 @@ node["hyperv"]["machines_to_create"].each do |virtualmachine|
   New-VM -Name "#{virtualmachine.name}" -MemoryStartupBytes #{virtualmachine.memory} -SwitchName "#{node["hyperv"]["switch"]}" -VHDPath "#{virtualmachine.destination_vhd}"
   EOH
   end
-
 end
